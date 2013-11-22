2988c5b5f61658096498788b00bc1b119cbcde3d
Dennis Schwerdel <schwerdel@googlemail.com>
2013-11-22 13:11:12 +0100

    Fixed missing /usr/lib/jvm

--- data/usr/share/btcvault/onload.sh
+++ data/usr/share/btcvault/onload.sh
@@ -2,6 +2,8 @@
 
 xdg-mime install /usr/share/btcvault/x-electrum-wallet.xml
 
+ln -s /usr/lib/jdk6 /usr/lib/jvm
+
 for patch in /usr/share/btcvault/patches/*.patch; do
   patch -p1 -d / -f < $patch
 done
